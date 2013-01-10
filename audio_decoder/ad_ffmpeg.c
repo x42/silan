731b3b9e9a06fad81402a25f1ac4ebb3545fe086
Robin Gareus <robin@gareus.org>
2013-01-10 16:58:28 +0100

    LGPL boilerplate for audio-decoder

--- audio_decoder/ad_ffmpeg.c
+++ audio_decoder/ad_ffmpeg.c
@@ -1,3 +1,22 @@
+/**
+   Copyright (C) 2011-2013 Robin Gareus <robin@gareus.org>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser Public License as published by
+   the Free Software Foundation; either version 2.1, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
 #include "config.h"
 #include <stdio.h>
 #include <stdlib.h>
