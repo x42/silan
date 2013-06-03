2fa3d8b47c5f882adb4bf8ce9e90a365d8d744cc
Robin Gareus <robin@gareus.org>
2013-06-03 21:51:00 +0200

    add scripts for static builds

---
+++ x-pbuildstatic.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+# this script creates a statically linked version
+# of ffmpeg, ffprobe and silan for GNU/Linux
+#
+# It is intended to run in a pristine chroot or VM of a minimal
+# debian system. see http://wiki.debian.org/cowbuilder
+#
+# This script
+#  - git clone the source to $SRC (default /usr/src)
+#  - build and install ffmpeg to $PFX (default ~/local/)
+#  - build silan and bundle it to /tmp/silan*.tgz
+#    (/tmp/ is fixed set by x-static.sh )
+#
+
+#use environment variables if set for SRC and PFX
+: ${SRC=/usr/src}
+: ${PFX=$HOME/local}
+
+if [ "$(id -u)" != "0" -a -z "$SUDO" ]; then
+	echo "This script must be run as root in pbuilder" 1>&2
+  echo "e.g sudo cowbuilder --architecture amd64 --distribution wheezy --bindmounts /tmp --execute $0"
+	exit 1
+fi
+
+$SUDO apt-get -y install git build-essential yasm \
+	libass-dev libbluray-dev libgmp3-dev \
+	libbz2-dev libfreetype6-dev libgsm1-dev liblzo2-dev \
+	libmp3lame-dev libopenjpeg-dev libopus-dev librtmp-dev \
+	libschroedinger-dev libspeex-dev libtheora-dev \
+	libvorbis-dev libvpx-dev libx264-dev \
+	libxvidcore-dev zlib1g-dev libflac-dev libogg-dev \
+	libsndfile1-dev automake
+
+cd $SRC
+git clone -b release/1.2 --depth 1 git://source.ffmpeg.org/ffmpeg
+git clone -b master git://github.com/x42/silan.git
+
+cd $SRC/silan
+VERSION=$(git describe --tags HEAD)
+git archive --format=tar --prefix=silan-${VERSION}/ HEAD | gzip -9 > /tmp/silan-${VERSION}.tar.gz
+
+cd $SRC/ffmpeg
+FFVERSION=$(git describe --tags)
+git archive --format=tar --prefix=ffmpeg-${FFVERSION}/ HEAD | gzip -9 > /tmp/ffmpeg-${FFVERSION}.tar.gz
+
+./configure --enable-gpl \
+	--enable-libmp3lame --enable-libx264 --enable-libxvid --enable-libtheora  --enable-libvorbis \
+	--enable-libvpx --enable-libopenjpeg --enable-libopus --enable-libschroedinger \
+	--enable-libspeex --enable-libbluray --enable-libgsm \
+	--disable-vaapi --disable-x11grab \
+	--disable-devices \
+	--enable-shared --enable-static --prefix=$PFX $@
+
+make -j4 || exit 1
+make install || exit 1
+
+cd $SRC/ffmpeg
+LIBDEPS=" \
+ libmp3lame.a \
+ libspeex.a \
+ libtheoraenc.a \
+ libtheoradec.a \
+ libogg.a \
+ libvorbis.a \
+ libvorbisenc.a \
+ libvorbisfile.a \
+ libschroedinger-1.0.a \
+ liborc-0.4.a \
+ libgsm.a \
+ libbluray.a \
+ libxvidcore.a \
+ libopus.a \
+ libbz2.a \
+ libvpx.a \
+ libopenjpeg.a \
+ libx264.a \
+ libz.a \
+ "
+
+SLIBS=""
+for SLIB in $LIBDEPS; do
+	echo -n "searching $SLIB.."
+	SL=`find /usr/lib -name "$SLIB"`
+	if test -z "$SL"; then
+		echo " not found."
+		exit 1
+	else
+		echo
+	fi
+	SLIBS="$SLIBS $SL"
+done
+
+cd $SRC/silan
+./x-static.sh || exit 1
+
+ls -l /tmp/silan*.tgz
---
+++ x-static.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+## compile a statically linked version of silan for linux
+## this requires a static build of ffmpeg; see x-pbuildstatic.sh
+##
+
+#path to the static ffmpeg installation
+: ${PFX=$HOME/local}
+#path to output directory -- /silan*.tgz will end up there
+: ${RESULT=/tmp}
+
+VERSION=$(git describe --tags HEAD || echo "X.X.X")
+TRIPLET=$(gcc -print-multiarch)
+OUTFN=silan-$TRIPLET-$VERSION
+
+LIBF=$PFX/lib
+BINF=$PFX/bin
+export PKG_CONFIG_PATH=${LIBF}/pkgconfig
+
+# ffmpeg needs this libs
+LIBDEPS=" \
+ libsndfile.a \
+ libFLAC.a \
+ libmp3lame.a \
+ libspeex.a \
+ libtheoraenc.a \
+ libtheoradec.a \
+ libogg.a \
+ libvorbis.a \
+ libvorbisenc.a \
+ libvorbisfile.a \
+ libschroedinger-1.0.a \
+ liborc-0.4.a \
+ libgsm.a \
+ libbluray.a \
+ libxvidcore.a \
+ libopus.a \
+ libbz2.a \
+ libvpx.a \
+ libopenjpeg.a \
+ libx264.a \
+ libz.a \
+ "
+
+if test "`hostname`" == "soyuz"; then
+	LIBDEPS="$LIBDEPS \
+ libX11.a \
+ libxcb.a \
+ libXau.a \
+ libXdmcp.a \
+ "
+fi
+
+# resolve paths to static libs on the system
+SLIBS=""
+for SLIB in $LIBDEPS; do
+	echo "searching $SLIB.."
+	SL=`find /usr/lib -name "$SLIB"`
+	if test -z "$SL"; then
+		echo "not found."
+		exit 1
+	fi
+	SLIBS="$SLIBS $SL"
+done
+
+LIBAD_SRC=" \
+  audio_decoder/ad_soundfile.c \
+  audio_decoder/ad_plugin.c \
+  audio_decoder/ad_ffmpeg.c \
+ "
+
+# compile silan
+#./autogen.sh
+mkdir -p tmp
+gcc -DNDEBUG \
+  -Wall -O3 \
+  -o tmp/$OUTFN -Iaudio_decoder -I. src/*.c ${LIBAD_SRC} \
+	`pkg-config --cflags libavcodec libavformat libavutil libpng libswscale` \
+	${CFLAGS} \
+	${LIBF}/libavformat.a \
+	${LIBF}/libavcodec.a \
+	${LIBF}/libswscale.a \
+	${LIBF}/libavdevice.a \
+	${LIBF}/libavutil.a \
+	\
+	$SLIBS \
+	-lm \
+|| exit 1
+
+strip tmp/$OUTFN
+ls -lh tmp/$OUTFN
+ldd tmp/$OUTFN
+
+# give any arg to disable bundle
+test -n "$1" && exit 1
+
+exit
+
+# build .tgz bundle
+rm -rf $RESULT/$OUTFN $RESULT/$OUTFN.tgz
+mkdir $RESULT/$OUTFN
+cp tmp/$OUTFN $RESULT/$OUTFN/silan
+cp README $RESULT/$OUTFN/README
+cp silan.1 $RESULT/$OUTFN/silan.1
+cd $RESULT/ ; tar czf $RESULT/$OUTFN.tgz $OUTFN ; cd -
+rm -rf $RESULT/$OUTFN
+ls -lh $RESULT/$OUTFN.tgz
